9553fed53753963f9eb8beab7772654738d63473
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-01 17:00:26 +0300

    tasklister mini project lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let taskForm = document.querySelector("#create-task-form")
+    taskForm.addEventListener("submit", e => {
+      e.preventDefault()
+      eventToDo(e.target.querySelector("#new-task-description").value)
+    taskForm.reset()
+    })
 });
+function eventToDo(e) {
+  let li = document.createElement('li')
+  li.textContent = e
+    let deleteButton = document.createElement('button')
+    deleteButton.textContent = "x"
+    deleteButton.addEventListener('click', deleteTask)
+  li.appendChild(deleteButton)
+  document.querySelector('#tasks').appendChild(li)
+
+}
+
+function deleteTask(e) {
+e.target.parentNode.remove()
+}
